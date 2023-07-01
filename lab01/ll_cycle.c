0177d343ac5b2a039235b0543b42c5d4ed2bfc4e
YangYY06 <1900011412@pku.edu.cn>
2023-07-02 00:07:57 +0800

    done with lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,14 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *tortoise = head, *hare= head;
+    while(hare) {
+        hare = hare->next;
+        if (!hare) return 0;
+        hare = hare->next;
+        tortoise = tortoise->next;
+        if (hare == tortoise)
+            return 1;
+    }
     return 0;
 }
